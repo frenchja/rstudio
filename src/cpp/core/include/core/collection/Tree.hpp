30b954825d44ee44cdfe7ea49e78e89c605bb393
JJ Allaire <jj@rstudio.org>
2013-01-03 16:45:56 -0500

    cleanup header

--- src/cpp/core/include/core/collection/Tree.hpp
+++ src/cpp/core/include/core/collection/Tree.hpp
@@ -11,32 +11,6 @@
  *
  */
 
-//	STL-like templated tree class.
-//
-// Copyright (C) 2001-2011 Kasper Peeters <kasper@phi-sci.com>
-// Distributed under the GNU General Public License version 3.
-//
-// When used together with the htmlcxx library to create 
-// HTML::Node template instances, the GNU Lesser General Public 
-// version 2 applies. Special permission to use tree.hh under
-// the LGPL for other projects can be requested from the author.
-
-/** \mainpage tree.hh
-    \author   Kasper Peeters
-    \version  2.81
-    \date     23-Aug-2011
-    \see      http://tree.phi-sci.com/
-    \see      http://tree.phi-sci.com/ChangeLog
-
-   The tree.hh library for C++ provides an STL-like container class
-   for n-ary trees, templated over the data stored at the
-   nodes. Various types of iterators are provided (post-order,
-   pre-order, and others). Where possible the access methods are
-   compatible with the STL or alternative algorithms are
-   available. 
-*/
-
-
 #ifndef CORE_COLLECTION_TREE_HPP
 #define CORE_COLLECTION_TREE_HPP
 
